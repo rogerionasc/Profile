a8f761be2a74de4cc3f2df31f7d6a49f03f7ae05
Rogerio Nascimento <rogerionascimentosantos@gmail.com>
2024-02-16 23:10:51 -0300

    Menu Toggle

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
     $('.fa-bars').on('click', function () {
         $('.menu-toggle').css('display', 'initial');
+        setTimeout(function () {
+            $('.menu-toggle').css('left', '0').css('display', 'initial');
+        }, 100); // 100 milissegundos de atraso
     });
 
     $('.fa-x').on('click', function () {
-        $('.menu-toggle').css('display', 'none');
+        $('.menu-toggle').css('left', '-61%');
+        setTimeout(function () {
+            $('.menu-toggle').css('display', 'none');
+        }, 500); // 500 milissegundos de atraso
     });
 
 });
