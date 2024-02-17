8c08bcf325410efe3f049cd19b0903edcbc80718
Rogerio Nascimento <rogerionascimentosantos@gmail.com>
2024-02-17 14:00:27 -0300

    Menu Toggle

--- src/js/custom.js
+++ src/js/custom.js
@@ -13,4 +13,9 @@ $(document).ready(function () {
         }, 500); // 500 milissegundos de atraso
     });
 
+    $(".project").mouseenter(function() {
+        $(this).find(".img-project").css("opacity", "1"); // Define a opacidade para 1 apenas na imagem dentro do elemento .project que está sendo interagido
+    }).mouseleave(function() {
+        $(this).find(".img-project").css("opacity", "0.5"); // Define a opacidade de volta para 0.5 na imagem dentro do elemento .project que está sendo interagido
+    });
 });
